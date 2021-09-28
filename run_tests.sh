d861e798973d2c032beb06a77cd49f7683aba8fa
NicEastvillage <nicoesterby@gmail.com>
2021-09-28 13:39:39 +0200

    Fix syntax error and add verifypn to gitignore.

--- run_tests.sh
+++ run_tests.sh
@@ -74,7 +74,7 @@ for TEST_TYPE in ReachabilityCardinality ; do
 		for Q in $(seq 1 $NQ ) ; do
 			
 			echo "	Q$Q"
-			CMD = "./binaries/$BIN $OPTIONS -x $Q $TEST_FOLDER/$MODEL/model.pnml $TEST_FOLDER/$MODEL/$TEST_TYPE.xml"
+			CMD="./binaries/$BIN $OPTIONS -x $Q $TEST_FOLDER/$MODEL/model.pnml $TEST_FOLDER/$MODEL/$TEST_TYPE.xml"
 			
 			# Execute test and store stdout in RES along with time and memory spent between @@@s
 			RES=$(eval "/usr/bin/time -f "@@@%e,%M@@@" timeout ${TIMING}m $CMD")
