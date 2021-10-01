fc971436be234527c347d5c521526adf9ae00483
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2021-10-01 10:50:54 +0200

    did stuff

--- graph_utility/stacked_bars.py
+++ graph_utility/stacked_bars.py
@@ -13,7 +13,11 @@ data_list = [pd.read_csv(path) for path in paths]
 num_rows = len(data_list[0].index)
 
 for index, data in enumerate(data_list):
-    sns.countplot(data=data, x='solved by query simplification').set(
-        title=f'{test_names[index]}-solved by simplification', xlabel='')
-    plt.savefig(f'graphs/solved_by_simplification_{test_names[index]}.png')
+    data.loc[data.answer != "None", "answer"] = "answered"
+    data.loc[data.answer == "None", "answer"] = "not-answered"
+
+    sns.countplot(data=data, x="answer", color='red').set(xlabel='')
+    sns.countplot(data=data, x='solved by query simplification', color='blue').set(xlabel='')
+
+    plt.savefig(f'graphs/stacked_bars_{test_names[index]}.png')
     plt.clf()
