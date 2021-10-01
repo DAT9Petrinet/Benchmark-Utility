7d93fbf82dd55f821cce4c4bbacf8218cce6c984
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2021-10-01 20:21:21 +0200

    fixes rule usage plot

--- graph_utility/rule_usage_barplot.py
+++ graph_utility/rule_usage_barplot.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import sys
@@ -14,6 +16,16 @@ rules = [column for column in (pd.read_csv(paths[0], index_col=0, nrows=0).colum
 
 for index, data in enumerate(data_list):
     data = data.drop(data[(data['solved by query simplification']) | (data.answer == 'NONE')].index)
-    sns.barplot(data=data[rules]).set(title=f'{test_names[index]}', ylabel='uses')
+    grouped_data = data.groupby(['model name'])[rules].agg('sum')
+    num_models = len(grouped_data)
+
+    #percentages = [(len(grouped_data[grouped_data[rule] > 0])) / num_models
+    #               for rule in rules]
+
+    percentages = ((grouped_data > 0)*1).mean()
+    percentages = (percentages.to_frame()).transpose()
+    print(percentages)
+
+    sns.barplot(data=percentages).set(title=f'{test_names[index]}', ylabel='uses in \%')
     plt.savefig(f'graphs/rule_usage_barplot_{test_names[index]}.png')
     plt.clf()
