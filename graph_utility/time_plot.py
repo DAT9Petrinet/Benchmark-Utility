1677d8b73f91d24e3043e272f9b4f4873cbc3019
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2021-10-02 12:38:51 +0200

    adds memory to time graph

--- graph_utility/time_plot.py
+++ graph_utility/time_plot.py
@@ -13,17 +13,28 @@ data_list = [pd.read_csv(path) for path in paths]
 
 combined_df = pd.DataFrame()
 for index, data in enumerate(data_list):
-    data = data.drop(data[(data['solved by query simplification']) | (data.answer == 'NONE')].index)
-    data = (((data.groupby(['model name'])['time'].agg('sum')).sort_values()).reset_index()).drop('model name', 1)
-    data.rename(columns={'time': f"{test_names[index]}"}, inplace=True)
-    if index == 0:
-        combined_df = data
-        continue
-    combined_df = combined_df.join(data)
+    time_data = data
+    time_data = time_data.drop(
+        time_data[(time_data['solved by query simplification']) | (time_data.answer == 'NONE')].index)
+    time_data = (((time_data.groupby(['model name'])['time'].agg('sum')).sort_values()).reset_index()).drop(
+        'model name', 1)
+    time_data.rename(columns={'time': f"{test_names[index]}-time"}, inplace=True)
 
-print(combined_df)
-sns.lineplot(data=combined_df).set(title=f'Model checking time per model, 16 queries each model',
-                                   ylabel='time taken in seconds',
+    memory_data = data
+    memory_data = memory_data.drop(
+        memory_data[(memory_data['solved by query simplification']) | (memory_data.answer == 'NONE')].index)
+    memory_data = (((memory_data.groupby(['model name'])['memory'].agg('sum')).sort_values()).reset_index()).drop(
+        'model name', 1)
+    memory_data.rename(columns={'memory': f"{test_names[index]}-memory"}, inplace=True)
+
+    memory_time_data = time_data.join(memory_data)
+    if index == 0:
+        combined_df = memory_time_data
+        continue
+    combined_df = combined_df.join(memory_time_data)
+
+sns.lineplot(data=combined_df).set(title=f'Model checking time and memory per model, 16 queries each model',
+                                   ylabel='',
                                    xlabel='models', yscale="log")
-plt.savefig(f'graphs/time_per_model.png')
+plt.savefig(f'graphs/time-memory_per_model.png')
 plt.clf()
