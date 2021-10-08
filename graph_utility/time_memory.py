6c8de02081ed198d53a5884915626164fa7de7ef
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2021-10-08 11:26:26 +0200

    moves legend in the 3 graphs, such that they are not in the way

--- graph_utility/time_memory.py
+++ graph_utility/time_memory.py
@@ -61,6 +61,7 @@ def plot(data_list, test_names):
         title=f'model checking time and memory per model',
         ylabel='seconds or kB',
         xlabel='models', yscale="log")
+    plt.legend(bbox_to_anchor=(1.02, 0.55), loc='best', borderaxespad=0)
 
-    plt.savefig('../graphs/time-memory_per_model.png')
+    plt.savefig('../graphs/time-memory_per_model.png', bbox_inches='tight')
     plt.clf()
