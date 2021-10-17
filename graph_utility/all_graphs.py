8697ce8ecd21a46b41c8e9c56c5b240b24d2d801
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2021-10-17 16:43:00 +0200

    adds time_memory_points graph

--- graph_utility/all_graphs.py
+++ graph_utility/all_graphs.py
@@ -10,6 +10,7 @@ import time_memory_combined
 import time_memory
 import reduction_points
 import total_reductions
+import time_memory_points
 
 
 def plot_all(data_list, test_names, graph_dir, correct_results_name):
@@ -57,6 +58,10 @@ def plot_all(data_list, test_names, graph_dir, correct_results_name):
     print(f"{graphs_made}/{num_graphs} graphs done")
 
     total_reductions.plot(data_list, test_names, graph_dir)
+    graphs_made = graphs_made + 1
+    print(f"{graphs_made}/{num_graphs} graphs done")
+
+    time_memory_points.plot(data_list, test_names, graph_dir, correct_results_name)
     graphs_made = graphs_made + 1
     print(f"{graphs_made}/{num_graphs} graphs done")
 
