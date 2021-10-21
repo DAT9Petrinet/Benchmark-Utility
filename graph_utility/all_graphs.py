a48037bcf24786bfa397e63bd8c1c0a4e405a4ca
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2021-10-21 13:25:09 +0200

    adds meta-graph

--- graph_utility/all_graphs.py
+++ graph_utility/all_graphs.py
@@ -1,8 +1,11 @@
 import os
 import sys
 import shutil
-
+import time
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 import answer_simplification_bars
 import rule_usage_absolute
 import rule_usage_percentage
@@ -29,65 +32,93 @@ def plot_all(data_list, test_names, graph_dir, experiment_to_compare_against_nam
     num_graphs = len(graphs)
     graphs_made = 0
 
+    times = dict()
+    time_when_started = time.time()
     # General graphs not fitting totally into the next categories
     answer_simplification_bars.plot(data_list, test_names, graph_dir)
+    times[graphs_made] = (round(time.time() * 1000) - time_when_started)
     graphs_made = graphs_made + 1
     print(f"{graphs_made}/{num_graphs} graphs made")
 
     best_overall.plot(data_list, test_names, graph_dir)
+    times[graphs_made] = (round(time.time() * 1000) - time_when_started)
     graphs_made = graphs_made + 1
     print(f"{graphs_made}/{num_graphs} graphs made")
 
     # Plots that has to do with application of rules
     rule_usage_absolute.plot(data_list, test_names, graph_dir + '\\rule-usage\\')
+    times[graphs_made] = (round(time.time() * 1000) - time_when_started)
     graphs_made = graphs_made + 1
     print(f"{graphs_made}/{num_graphs} graphs made")
 
     rule_usage_percentage.plot(data_list, test_names, graph_dir + '\\rule-usage\\')
+    times[graphs_made] = (round(time.time() * 1000) - time_when_started)
     graphs_made = graphs_made + 1
     print(f"{graphs_made}/{num_graphs} graphs made")
 
     rule_usage_absolute_models.plot(data_list, test_names, graph_dir + '\\rule-usage\\')
+    times[graphs_made] = (round(time.time() * 1000) - time_when_started)
     graphs_made = graphs_made + 1
     print(f"{graphs_made}/{num_graphs} graphs made")
 
     # Stuff to do with time and memory
     time_memory_combined.plot(data_list, test_names, graph_dir + '\\time-memory\\')
+    times[graphs_made] = (round(time.time() * 1000) - time_when_started)
     graphs_made = graphs_made + 1
     print(f"{graphs_made}/{num_graphs} graphs made")
 
     metrics = ['time', 'memory']
     for metric in metrics:
         time_memory_lines.plot(data_list, test_names, graph_dir + '\\time-memory\\', metric)
+    times[graphs_made] = (round(time.time() * 1000) - time_when_started)
     graphs_made = graphs_made + 1
     print(f"{graphs_made}/{num_graphs} graphs made")
 
     time_memory_points.plot(data_list, test_names, graph_dir + '\\time-memory\\', experiment_to_compare_against_name)
+    times[graphs_made] = (round(time.time() * 1000) - time_when_started)
     graphs_made = graphs_made + 1
     print(f"{graphs_made}/{num_graphs} graphs made")
 
     # Stuff to do with reduction/size
     reduced_size.plot(data_list, test_names, graph_dir + '\\reductions\\')
+    times[graphs_made] = (round(time.time() * 1000) - time_when_started)
     graphs_made = graphs_made + 1
     print(f"{graphs_made}/{num_graphs} graphs made")
 
     reduction_points.plot(data_list, test_names, graph_dir + '\\reductions\\', experiment_to_compare_against_name)
+    times[graphs_made] = (round(time.time() * 1000) - time_when_started)
     graphs_made = graphs_made + 1
     print(f"{graphs_made}/{num_graphs} graphs made")
 
     total_reductions.plot(data_list, test_names, graph_dir + '\\reductions\\')
+    times[graphs_made] = (round(time.time() * 1000) - time_when_started)
     graphs_made = graphs_made + 1
     print(f"{graphs_made}/{num_graphs} graphs made")
 
     # Stuff to do with ratios
     time_size_ratios_lineplots.plot(data_list, test_names, graph_dir + '\\size-ratios\\',
                                     experiment_to_compare_against_name)
+    times[graphs_made] = (round(time.time() * 1000) - time_when_started)
     graphs_made = graphs_made + 1
     print(f"{graphs_made}/{num_graphs} graphs made")
 
     time_size_avg_ratios.plot(data_list, test_names, graph_dir + '\\size-ratios\\', experiment_to_compare_against_name)
+    times[graphs_made] = (round(time.time() * 1000) - time_when_started)
     graphs_made = graphs_made + 1
     print(f"{graphs_made}/{num_graphs} graphs made")
+
+    # Print meme graph
+    df = pd.DataFrame.from_dict(times, orient='index')
+    print(df)
+    plot = sns.lineplot(data=df)
+    plot.set(
+        title=f'Time spent making graphs',
+        ylabel=f'time in ms',
+        xlabel='number of models', yscale="log")
+    plt.legend(bbox_to_anchor=(1.02, 1), loc='best', borderaxespad=0)
+
+    plt.savefig(graph_dir + 'time_spent_making_graphs', bbox_inches='tight')
+    plt.clf()
 
 
 if __name__ == "__main__":
