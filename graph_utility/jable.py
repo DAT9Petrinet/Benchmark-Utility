1208c800f29eba715cc2443a2268630b6085ea22
NicEastvillage <nicoesterby@gmail.com>
2021-11-11 14:13:46 +0100

    Add script to make jiri's table (#78)
    * Add script to make the Jable
    
    * Clean up.

---
+++ graph_utility/jable.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+import pandas as pd
+
+
+data_dir = Path(__file__).parent.parent / "saved"
+
+csvs = [pd.read_csv(csv) for csv in data_dir.glob("*.csv")]
+exp_names = [csv.stem for csv in data_dir.glob("*.csv")]
+
+for i, csv in enumerate(csvs):
+    csv.set_index(["model name", "query index"], inplace=True)
+    csv.rename(columns={col: f"{exp_names[i]}@{col}" for col in csv.columns}, inplace=True)
+everything = pd.concat(csvs, axis=1)
+everything.sort_index(level=0, inplace=True)
+
+everything.to_csv("../saved/everything/everything.csv", sep=";", decimal=",")
