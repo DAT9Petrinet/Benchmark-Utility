3c1d04253ec928677a8ab204c3b051c5792cde4d
NicEastvillage <nicoesterby@gmail.com>
2021-11-11 14:05:29 +0100

    Clean up.

--- graph_utility/jable.py
+++ graph_utility/jable.py
@@ -7,10 +7,11 @@ data_dir = Path(__file__).parent.parent / "saved"
 
 csvs = [pd.read_csv(csv) for csv in data_dir.glob("*.csv")]
 exp_names = [csv.stem for csv in data_dir.glob("*.csv")]
+
 for i, csv in enumerate(csvs):
     csv.set_index(["model name", "query index"], inplace=True)
     csv.rename(columns={col: f"{exp_names[i]}@{col}" for col in csv.columns}, inplace=True)
 everything = pd.concat(csvs, axis=1)
 everything.sort_index(level=0, inplace=True)
-print(everything)
+
 everything.to_csv("../saved/everything/everything.csv", sep=";", decimal=",")
