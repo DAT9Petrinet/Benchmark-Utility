9bfa2c109fa6b698d611af7b5994f75c13a95573
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2021-11-29 11:30:00 +0100

    remove meme

--- graph_utility/all_graphs.py
+++ graph_utility/all_graphs.py
@@ -25,22 +25,14 @@ def plot_all(data_list, test_names, graph_dir, experiment_to_compare_against_nam
               '.py' in filename and filename not in ['all_graphs.py', 'jable.py', 'utility.py']]
     num_graphs = len(graphs)
     graphs_made = 0
-    times = dict()
-    time_when_started = time.time()
-
-    def update_globals(experiment_name, graphs_made):
-        times[experiment_name] = (round(time.time()) - time_when_started)
-        graphs_made += 1
-        print(f"{graphs_made}/{num_graphs} graphs made")
-        return graphs_made
 
     # Sanitise the data
     data_list = utility.sanitise_df_list(data_list)
 
     # General graphs not fitting totally into the next categories
-    time_when_started = time.time()
     answer_simplification_bars.plot(data_list, test_names, graph_dir + '\\best-experiment\\')
-    graphs_made = update_globals('answer/simplification bars', graphs_made)
+    graphs_made += 1
+    print(f"{graphs_made}/{num_graphs} graphs made")
 
     '''time_when_started = time.time()
     for keep_largest_percent in [0.1]:
@@ -50,48 +42,25 @@ def plot_all(data_list, test_names, graph_dir, experiment_to_compare_against_nam
                                experiment_to_compare_against_name, keep_largest_percent, how_much_better)
     graphs_made = update_globals('best experiment overall', graphs_made)'''
 
-    time_when_started = time.time()
+
     # Plots that has to do with application of rules
     rule_usage.plot(data_list, test_names, graph_dir + '\\rule-usage\\')
-    graphs_made = update_globals('rule usages', graphs_made)
+    graphs_made += 1
+    print(f"{graphs_made}/{num_graphs} graphs made")
 
-    time_when_started = time.time()
     metrics = ['verification time', 'verification memory', 'state space size', 'reduce time', 'reduced size',
                'total time']
     for metric in metrics:
         for percentage in [0.01, 0.025, 0.05, 0.1, 0.5, 1]:
             time_memory_lines.plot(data_list, test_names, graph_dir + '\\lines\\' + metric.replace(" ", "-") + '\\',
                                    metric, percentage)
-    graphs_made = update_globals('time/memory lines', graphs_made)
+    graphs_made += 1
+    print(f"{graphs_made}/{num_graphs} graphs made")
 
-    time_when_started = time.time()
+
     total_reductions.plot(data_list, test_names, graph_dir + '\\reductions\\')
-    graphs_made = update_globals('total reductions', graphs_made)
-
-    # Print meme graph
-    df = pd.DataFrame.from_dict(times, orient='index')
-    # Plot the plot
-    sns.set_theme(style="darkgrid", palette="pastel")
-    plot = df.plot(kind='barh', width=0.75, linewidth=2, figsize=(10, 10), legend=False)
-    plt.xscale('linear')
-    plt.xlabel("seconds")
-    plt.ylabel('graphs')
-    plt.title('Time in seconds spent making graphs')
-
-    # Find max width, in order to move the very small numbers away from the bars
-    max_width = 0
-    for p in plot.patches:
-        left, bottom, width, height = p.get_bbox().bounds
-        max_width = max(width, max_width)
-    # Plot the numbers in the bars
-    for p in plot.patches:
-        left, bottom, width, height = p.get_bbox().bounds
-        plot.annotate(int(width), xy=(left + width / 2, bottom + height / 2),
-                      ha='center', va='center')
-
-    plt.savefig(graph_dir + f'time_spent_making_graphs.png', bbox_inches='tight')
-    plt.clf()
-
+    graphs_made += 1
+    print(f"{graphs_made}/{num_graphs} graphs made")
 
 if __name__ == "__main__":
     # Results used for comparisons
