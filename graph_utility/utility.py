ec43ea591caeedeed5db5033fd0d1322cfd2ee14
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2022-01-07 15:38:18 +0100

    fix some legend

--- graph_utility/utility.py
+++ graph_utility/utility.py
@@ -240,6 +240,18 @@ def remove_errors_datalist(data_list):
         data.drop(data[data['answer'] == 'ERR'].index, inplace=True)
     return data_list
 
+def rename_test_name_for_paper_presentation(test_names):
+    new_test_names = {}
+    for test_name in test_names:
+        if "fixed" in test_name:
+            new_test_name = test_name.replace('fixed', '')
+        else:
+            new_test_name = test_name.replace('with-', 'base.')
+
+        new_test_name = f"({new_test_name})⃰"
+
+        new_test_names[test_name] = new_test_name
+    return new_test_names
 
 '''
 def split_into_all_with_without(df):
